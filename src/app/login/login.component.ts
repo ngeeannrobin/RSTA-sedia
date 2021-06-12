8fb98f990ecd3ca48b1c4c1803e54bad3e20505d
ngeeannrobin <s10179209@connect.np.edu.sg>
2021-06-12 14:18:47 +0800

    add login feature

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -7,16 +8,53 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  message: string = "_";
   constructor(
+    private router: Router,
     private auth: AuthService
   ) { }
 
   ngOnInit(): void {
   }
 
-  signIn(){
-    this.auth.GoogleAuth();
+  async signInWithGoogle(){
+    // this.auth.GoogleAuth();
+    this.auth.CheckLogin().then(loggedIn=>{
+      if (!loggedIn){
+        this.message = "Google Pop-up opened."
+        this.auth.GoogleAuth().then(_ => {
+          console.log(_);
+        })
+      } else {
+        this.redirect();
+      }
+    }) 
+  }
+
+  async Checker(){
+    let checking = true;
+    while (checking){
+      await this.Delay(1000);
+      this.auth.CheckLogin().then(loggedIn=>{
+        if (loggedIn){
+          checking=false;
+          this.redirect();
+          
+        }
+      })
+    }
+  }
+
+  async redirect(){
+    this.message = "Redirecting...";
+    // this.redirec = true;
+    // this.displayProgressSpinner = false;
+    await this.Delay(300);
+    this.router.navigate(["/MainMenu"]);
+  }
+
+  async Delay(ms){
+    return new Promise( resolve => setTimeout(resolve, ms));
   }
 
 }
