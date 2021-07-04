2a821193bdf4264df234a190aab83fe01fa9a0ff
ngeeannrobin <s10179209@connect.np.edu.sg>
2021-07-04 18:59:25 +0800

    change navbar ui

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 export class NavBarComponent implements OnInit {
 
   constructor(private router: Router) { }
-
+  @Input() darkMode: boolean = false;
   ngOnInit(): void {
   }
 
