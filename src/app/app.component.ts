e8019cbe2265064af3dd73225f16785c6da25ce2
ngeeannrobin <s10179209@connect.np.edu.sg>
2021-07-04 12:34:16 +0800

    add automatic update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RSTA-sedia';
+  constructor(private updates: SwUpdate) {
+    this.updates.available.subscribe(event => {
+      this.doAppUpdate();
+    })
+  }
+  doAppUpdate() {
+    this.updates.activateUpdate().then(() => document.location.reload());
+  }
 }
+
