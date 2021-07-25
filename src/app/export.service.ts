97bc5de669d34b2e1c78909980bf390344b1657d
ngeeannrobin <s10179209@connect.np.edu.sg>
2021-07-25 14:54:04 +0800

    add export to excel function

---
+++ src/app/export.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { Workbook } from 'exceljs';
+import * as fs from 'file-saver';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExportService {
+
+  constructor() { }
+
+
+  ExportExcel(data,dateString){
+    console.log(data);
+    let workbook = new Workbook();
+    let worksheet = workbook.addWorksheet("Bibo");
+
+    // Declare headers
+    worksheet.columns = [
+      { header: 'Rank', key: 'rank'},
+      { header: 'Name', key: 'name'},
+      { header: 'Book In Date', key: 'bid'},
+      { header: 'Book In Time', key: 'bit'},
+      { header: 'Book Out Date', key: 'bod'},
+      { header: 'Book Out Time', key: 'bot'}
+    ];
+
+    // Insert data
+    let longestName = 0;
+    data.forEach(record => {
+      let row = [];
+      row[0] = record.rank;
+      row[1] = record.name;
+      row[record.in?2:4] = this.ConvertDateToDate(record.time);
+      row[record.in?3:5] = this.ConvertDateToHour(record.time);
+      worksheet.addRow(row);
+      longestName = Math.max(longestName,record.name.length);
+    });
+
+    // Apply styles
+    worksheet.getRows(1,1)[0].font = {bold: true};
+
+    worksheet.getColumn("rank").width = 5;
+    worksheet.getColumn("name").width = longestName+2;
+
+
+
+    ["A", "C","D","E","F"].forEach(col=>{
+      worksheet.getColumn(col).alignment = {horizontal: "center",}
+    });
+
+    ["A1","B1","C1","D1","E1","F1"].forEach(cell => {
+      worksheet.getCell(cell).alignment = {wrapText: true, vertical: 'middle', horizontal: 'center'}
+    });
+
+    workbook.xlsx.writeBuffer().then((data) => {
+      let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      fs.saveAs(blob, `Bibo-${dateString}.xlsx`);
+    });
+  }
+
+  ConvertDateToDate(ts){
+    const dt = new Date(ts.seconds*1000);
+    return `${dt.getDate().toString().padStart(2,"0")}${(dt.getMonth() + 1).toString().padStart(2,"0")}${dt.getFullYear().toString().substr(2,2)}`;
+  }
+  ConvertDateToHour(ts){
+    const dt = new Date(ts.seconds*1000);
+    return `${dt.getHours().toString().padStart(2,"0")}${dt.getMinutes().toString().padStart(2,"0")}`
+  }
+}
--- src/app/view-coy-bibo/view-coy-bibo.component.ts
+++ src/app/view-coy-bibo/view-coy-bibo.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { BiboService } from '../bibo.service';
+import { ExportService } from '../export.service';
 
 @Component({
   selector: 'app-view-coy-bibo',
@@ -9,7 +10,7 @@ import { BiboService } from '../bibo.service';
 })
 export class ViewCoyBiboComponent implements OnInit {
 
-  constructor(private auth: AuthService, private bibo: BiboService) { }
+  constructor(private auth: AuthService, private bibo: BiboService, private xport: ExportService) { }
   data:any = {};
   dateString: string;
   date:Date = new Date();
@@ -59,4 +60,17 @@ export class ViewCoyBiboComponent implements OnInit {
     }
   }
 
+  Download(){
+    this.xport.ExportExcel(this.data,this.ConvertDateToTitle(this.date));
+  }
+
+
+  ConvertDateToHours(ts){
+    const dt = new Date(ts.seconds*1000);
+    return `${dt.getHours().toString().padStart(2,"0")}${dt.getMinutes().toString().padStart(2,"0")} hrs`
+  }
+
+  NoRecord(){
+    return Object.keys(this.data).length===0
+  }
 }
