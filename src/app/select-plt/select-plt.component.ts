0b9988ec85e9d73f58659695fcfeacc5eb5e4a6c
Robin DeBank <bindebank@gmail.com>
2021-10-31 14:39:46 +0800

    transit coy

--- src/app/select-plt/select-plt.component.ts
+++ src/app/select-plt/select-plt.component.ts
@@ -10,7 +10,7 @@ export class SelectPltComponent implements OnInit {
   constructor() { }
   plt: number;
   bookIn: boolean;
-  plts: Array<any> = [["Coy HQ","coyhq.png"],["Platoon 1","plt1.png"],["Platoon 2","plt2.png"],["Platoon 3","plt3.png"]];
+  plts: Array<any> = [["Coy HQ","coyhq.png"],["Platoon 1","plt1.png"],["Platoon 2","plt2.png"]];
 
   @Output() final: EventEmitter<number> = new EventEmitter();
 
